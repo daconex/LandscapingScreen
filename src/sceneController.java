5261d724cfd5c8a95abadee147d5a7af781476df
David Corbin <daconex@gmail.com>
2015-03-16 02:34:51 +0000

    Fixed indentation

--- src/sceneController.java
+++ src/sceneController.java
@@ -15,15 +15,15 @@ public class sceneController implements Initializable  {
 		System.out.println("Hello!");
 	}
 
-		@Override
-		public void initialize(URL location, ResourceBundle resources) {
-			// TODO Auto-generated method stub
-			assert myButton != null : "fx:id=\"myButton\" was not injected: check your FXML file 'simple.fxml'.";
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		assert myButton != null : "fx:id=\"myButton\" was not injected: check your FXML file 'simple.fxml'.";
 
-		    // initialize your logic here: all @FXML variables will have been injected
-		}
+		// initialize your logic here: all @FXML variables will have been injected
+	}
 
-		public void showGreeting(ActionEvent event){ 
-			System.out.println("Hello!");
-		}
+	public void showGreeting(ActionEvent event){ 
+		System.out.println("Hello!");
+	}
 }
